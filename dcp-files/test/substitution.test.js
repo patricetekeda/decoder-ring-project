639f9025759f4093d9afe99b5bc754d8d8ee3b27
Patrice Tekeda <patricetekeda@gmail.com>
2022-09-21 22:55:47 -0400

    new project

---
+++ dcp-files/test/substitution.test.js
@@ -0,0 +1,18 @@
+// Write your tests here!
+const { substitution } = require("../src/substitution.js");
+const expect = require("chai").expect;
+
+describe("substitution", () => {
+  it("should encode a message by using the given substitution alphabet", () => {
+    const expected = "jrufscpw";
+    const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal(expected);
+  });
+  it("should have all unique parameters", () => {
+    const actual = substitution(
+      "You are an excellent spy",
+      "abcabcabcabcabcabcabcabcab"
+    );
+    expect(actual).to.be.false;
+  });
+});
